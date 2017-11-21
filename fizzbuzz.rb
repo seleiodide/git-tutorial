6f7847c66b4fc15cd0f18741ec25cdd0f3cf3bcf
seleiode <j1417@matsue-ct.jp>
2017-11-21 10:11:50 +0900

    edit fizzbuzz.rb (else if -> elsif

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-100.times {|i|
+100.times do |i|
 	if (i % 3 == 0 && i % 5 == 0) then
 		puts "FizzBuzz\n"
-	else if (i % 3 == 0) then
+	elsif (i % 3 == 0) then
 		puts "Fizz\n"
-	else if (i % 5 == 0) then
+	elsif (i % 5 == 0) then
 		puts "Buzz\n"
 	else 
 		puts i
 	end
-}
+end
